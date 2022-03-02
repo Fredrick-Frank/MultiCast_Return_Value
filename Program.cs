fddbf5f1b3e71ea0f4c0ad10ab604df00214bc6a
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-03-02 13:34:57 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiCastReturnValue
+{
+    public delegate int MyDelegate(); //declaring a delegate
+
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate del1 = ClassA.MethodA; //set target method
+            MyDelegate del2 = ClassB.MethodB;
+
+            MyDelegate del = del1 + del2;
+            Console.WriteLine(del()); //returns 200
+        }
+        class ClassA
+        {
+            public static int MethodA()
+            {
+                return 100;
+            }
+        }
+        class ClassB
+        {
+            public static int MethodB()
+            {
+                return 200;
+            }
+        }
+    }
+}
